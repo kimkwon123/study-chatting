0a78880c6bcda906e8e7c2518f5d0a24d7687549
kmg159753 <mingyugim584@gmail.com>
2023-07-01 20:57:19 +0900

    update : 파일 깨짐 수정

--- src/main/java/com/chatting/chatting/global/model/type/ErrorCode.java
+++ src/main/java/com/chatting/chatting/global/model/type/ErrorCode.java
@@ -13,17 +13,15 @@ public enum ErrorCode {
     SAME_EMAIL(HttpStatus.BAD_REQUEST,"동일한 이메일로 가입을 하셨습니다."),
     NO_SUCCESSKEY(HttpStatus.BAD_REQUEST, "이메일 인증키가 맞지 않습니다."),
     NO_SUCCESS(HttpStatus.BAD_REQUEST, "이메일 인증 먼저 해주세요."),
-
-    //로그인
-    WRONG_TOKEN(HttpStatus.BAD_REQUEST, "잘못된 토큰 값입니다."),
-
-    //상품
-    WRONG_SHOP_USER(HttpStatus.UNAUTHORIZED, "해당 상품은 작성자만 수정/삭제 할 수 있습니다."),
-    NO_SHOP(HttpStatus.BAD_REQUEST, "잘못된 상품 접근 입니다."),
-
-    //파일
-    ERROR_FILE(HttpStatus.NOT_FOUND, "해당 파일을 삭제하지 못하였습니다."),
-    NO_FILE(HttpStatus.NOT_FOUND, "해당 파일을 찾을수 없습니다.")
+    NO_INQUIRY_TITLE(HttpStatus.BAD_REQUEST, "제목을 입력해주세요"),
+    NO_INQUIRY_CONTENT(HttpStatus.BAD_REQUEST, "내용을 입력해주세요"),
+    NO_INQUIRY_CATEGORY(HttpStatus.BAD_REQUEST, "문의 카테고리를 선택하세요"),
+    NO_AUTHORITY_TO_INQUIRY(HttpStatus.BAD_REQUEST, "문의에 대한 수정/삭제 권한이 없습니다."),
+    NO_INQUIRY(HttpStatus.BAD_REQUEST, "존재하지 않는 문의글 입니다."),
+    NO_REPLY_CONTENT(HttpStatus.BAD_REQUEST, "내용을 입력하세요"),
+    NO_ADMIN(HttpStatus.BAD_REQUEST, "당신은 관리자가 아닙니다."),
+    NO_REPLY(HttpStatus.BAD_REQUEST, "문의에 대한 답변이 없습니다."),
+    NO_AUTHORITY_TO_REPLY(HttpStatus.BAD_REQUEST, "답변에 대한 수정/삭제 권한이 없습니다.")
     ;
 
     private final HttpStatus status;
