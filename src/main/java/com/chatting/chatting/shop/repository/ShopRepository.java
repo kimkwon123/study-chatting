c19a19b507d91ca3016bd3b226e521ec0d4d238e
Domae-back-end <ty_ty123@naver.com>
2023-07-04 16:47:28 +0900

    update: Shop 연관관계 수정 ShopRoom

--- src/main/java/com/chatting/chatting/shop/repository/ShopRepository.java
+++ src/main/java/com/chatting/chatting/shop/repository/ShopRepository.java
@@ -1,7 +1,11 @@
 package com.chatting.chatting.shop.repository;
 
+import com.chatting.chatting.certification.model.entity.User;
 import com.chatting.chatting.shop.model.entity.Shop;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ShopRepository extends JpaRepository<Shop, Long> {
+
 }
